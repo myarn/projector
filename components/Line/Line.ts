a08745cb254bf6242a4e410b69ed215843396bfa
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2023-04-28 21:26:12 +0900

    fix: Fix indent.

--- components/Line/Line.ts
+++ components/Line/Line.ts
@@ -2,40 +2,40 @@ import { FunctionalText, Text, TextComponent , Projector } from '../../mod.ts';
 
 export class Line {
   texts: TextComponent[] = [];
- constructor (
-   protected projector: Projector
- ) {
-   this.projector.lines.push(this);
- }
+  constructor (
+    protected projector: Projector
+  ) {
+    this.projector.lines.push(this);
+  }
 
- update () {
-   this.projector.updateLine(this);
- }
+  update () {
+    this.projector.updateLine(this);
+  }
 
- get renderedText (): string {
-   return this.texts.map((text) => text.renderedText).join('');
- }
+  get renderedText (): string {
+    return this.texts.map((text) => text.renderedText).join('');
+  }
 
- addText (text: string | TextComponent): TextComponent {
-   if (text instanceof Text) {
-     text.addParentLine(this);
-     this.texts.push(text);
-     this.update();
-     return text;
-   } else {
-     return this.addText(new Text(text));
-   }
- }
+  addText (text: string | TextComponent): TextComponent {
+    if (text instanceof Text) {
+      text.addParentLine(this);
+      this.texts.push(text);
+      this.update();
+      return text;
+    } else {
+      return this.addText(new Text(text));
+    }
+  }
 
- addTemplateText ([texts, renderFunction]: [Text[], () => string]) {
-   for (const text of texts) {
-     text.addParentLine(this);
-   }
+  addTemplateText ([texts, renderFunction]: [Text[], () => string]) {
+    for (const text of texts) {
+      text.addParentLine(this);
+    }
 
-   this.addText(new FunctionalText(renderFunction));
- }
+    this.addText(new FunctionalText(renderFunction));
+  }
 
- addTexts (...texts: (TextComponent | string)[]): TextComponent[] {
-   return texts.map(text => this.addText(text));
- }
+  addTexts (...texts: (TextComponent | string)[]): TextComponent[] {
+    return texts.map(text => this.addText(text));
+  }
 }
